c8efa612421fdc4f6e0bc0b8429ca35febf087a1
lauhonyanhk123-creator <lauhonyanhk123-creator@users.noreply.github.com>
2026-05-10 23:21:31 +0000

    feat: Test Repo with Webapp-Testing Skill
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-9ba2d447'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1lh5il1a8oo"
+    "revision": "0.37v63skfs08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
